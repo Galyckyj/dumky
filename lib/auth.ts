94971dbfe516951dcd59ed3312439e14dc91bfa7
Danylo <40806031+Galyckyj@users.noreply.github.com>
2024-12-25 23:44:35 +0200

    Update

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,7 @@ import db from "./db";
 import { compare } from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
